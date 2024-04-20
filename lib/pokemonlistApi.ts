176d32d70c66a6561c5f8ceb43fd3456c94003fd
Dz.dev <darwinbjordan@gmail.com>
2024-04-20 17:11:03 +0800

    finish nostalgic pokedex app

---
+++ lib/pokemonlistApi.ts
@@ -0,0 +1,15 @@
+// get pokemon list collection from API
+
+const fetchPokemon = "https://pokeapi.co/api/v2/";
+
+export async function getPokemonList() {
+  const response = await fetch(fetchPokemon + "pokemon?limit=250&offset=0");
+  const data = await response.json();
+  return data.results;
+}
+
+export async function getPokemon(name: string) {
+  const response = await fetch(fetchPokemon + "pokemon/" + name);
+  const data = await response.json();
+  return data;
+}
